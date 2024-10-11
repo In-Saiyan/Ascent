9464105584bb7b2da011c52461e3e16459aed3b4
In-Saiyan <as9099401@gmail.com>
2024-10-12 02:16:01 +0530

    fixed EVRYTHING

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-window.onscroll = function() {
+window.onscroll = function () {
     const scrollPosition = window.scrollY;
     const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const triggerPoint = pageHeight / 8; 
+    const triggerPoint = pageHeight / 8;
 
     if (scrollPosition > triggerPoint * 7) {
-        document.body.style.backgroundColor = "black"; 
+        document.body.style.backgroundColor = "black";
     } else if (scrollPosition > triggerPoint * 6) {
-        document.body.style.backgroundColor = "#2B1C1B"; 
+        document.body.style.backgroundColor = "#2B1C1B";
     } else if (scrollPosition > triggerPoint * 5) {
         document.body.style.backgroundColor = "#3B2524";
     } else if (scrollPosition > triggerPoint * 4) {
-        document.body.style.backgroundColor = "#4B2E2A"; 
+        document.body.style.backgroundColor = "#4B2E2A";
     } else if (scrollPosition > triggerPoint * 3) {
         document.body.style.backgroundColor = "#6B3E3A";
     } else if (scrollPosition > triggerPoint * 2) {
@@ -18,18 +18,28 @@ window.onscroll = function() {
     } else if (scrollPosition > triggerPoint) {
         document.body.style.backgroundColor = "#A46B5A";
     } else {
-        document.body.style.backgroundColor = "#BE806B"; 
+        document.body.style.backgroundColor = "#BE806B";
     }
 
     const topbar = document.getElementById("topbar");
-    const scrollPosition2 = window.scrollY;
     const halfway = document.documentElement.scrollHeight / 5;
-    const body = document.body;
 
-    if (scrollPosition2 > halfway) {
-        topbar.style.display = "block";
-        body.style.color = "white";
+    if (scrollPosition > halfway) {
+        topbar.style.display = "flex";  
+        document.body.style.color = "white"; 
     } else {
-        topbar.style.display = "none";
+        topbar.style.display = "none";  
+        document.body.style.color = "black"; 
+    }
+};
+
+
+window.onload = function () {
+    const topbar = document.getElementById("topbar");
+    const scrollPosition = window.scrollY;
+    const halfway = document.documentElement.scrollHeight / 5;
+
+    if (scrollPosition > halfway) {
+        topbar.style.display = "flex";  
     }
 };
