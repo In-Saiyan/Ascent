723767c6b3cc0044941612df00a08fa493b8c3da
ThunderBlaze <darkthunderstormshiva@gmail.com>
2024-12-08 06:09:19 +0530

    Changed Whole Color Scheme and Fixed Many Things

--- src/js/index.js
+++ src/js/index.js
@@ -3,23 +3,23 @@ window.onscroll = function () {
     const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
     const triggerPoint = pageHeight / 8;
 
-    if (scrollPosition > triggerPoint * 7) {
-        document.body.style.backgroundColor = "black";
-    } else if (scrollPosition > triggerPoint * 6) {
-        document.body.style.backgroundColor = "#2B1C1B";
-    } else if (scrollPosition > triggerPoint * 5) {
-        document.body.style.backgroundColor = "#3B2524";
-    } else if (scrollPosition > triggerPoint * 4) {
-        document.body.style.backgroundColor = "#4B2E2A";
-    } else if (scrollPosition > triggerPoint * 3) {
-        document.body.style.backgroundColor = "#6B3E3A";
-    } else if (scrollPosition > triggerPoint * 2) {
-        document.body.style.backgroundColor = "#8C5A4A";
-    } else if (scrollPosition > triggerPoint) {
-        document.body.style.backgroundColor = "#A46B5A";
-    } else {
-        document.body.style.backgroundColor = "#BE806B";
-    }
+    // if (scrollPosition > triggerPoint * 7) {
+    //     document.body.style.backgroundColor = "black";
+    // } else if (scrollPosition > triggerPoint * 6) {
+    //     document.body.style.backgroundColor = "#2B1C1B";
+    // } else if (scrollPosition > triggerPoint * 5) {
+    //     document.body.style.backgroundColor = "#3B2524";
+    // } else if (scrollPosition > triggerPoint * 4) {
+    //     document.body.style.backgroundColor = "#4B2E2A";
+    // } else if (scrollPosition > triggerPoint * 3) {
+    //     document.body.style.backgroundColor = "#6B3E3A";
+    // } else if (scrollPosition > triggerPoint * 2) {
+    //     document.body.style.backgroundColor = "#8C5A4A";
+    // } else if (scrollPosition > triggerPoint) {
+    //     document.body.style.backgroundColor = "#A46B5A";
+    // } else {
+    //     document.body.style.backgroundColor = "#BE806B";
+    // }
 
     const topbar = document.getElementById("topbar");
     const halfway = document.documentElement.scrollHeight / 9;
